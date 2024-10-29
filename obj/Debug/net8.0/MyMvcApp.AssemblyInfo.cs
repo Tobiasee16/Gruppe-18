90a49ef3326079fab79f2b9be6e5cc6abd6f2697
Tobiasee16 <engedaltobias@icloud.com>
2024-10-29 14:38:02 +0100

    fikset

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6442ffb3704f0577e4415a1f881884130b166afb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de5ba479a269cf0b7388b1839288d431e3f37fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
